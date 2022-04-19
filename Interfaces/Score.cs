953b32cf53ce07f6b95c3401376c38d20ff8d2ea
d.unarov <d.unarov@okko.tv>
2022-04-19 22:14:58 +0300

    feat: Score struct

---
+++ Interfaces/Score.cs
@@ -0,0 +1,14 @@
+﻿namespace Interfaces;
+public struct Score
+{
+    public Score(int ownerTeam, int guestTeam)
+    {
+        OwnerTeam = ownerTeam;
+        GuestTeam = guestTeam;
+    }
+
+    public int OwnerTeam { get; }
+    public int GuestTeam { get; }
+
+    public override string ToString() => $"({OwnerTeam}, {GuestTeam})";
+}
