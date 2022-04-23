490f836cc25f1787bc0afd2702792bdb1bb93c22
d.unarov <d.unarov@okko.tv>
2022-04-23 22:36:32 +0300

    feat: Score Equals override

--- Interfaces/Score.cs
+++ Interfaces/Score.cs
@@ -1,5 +1,5 @@
 ﻿namespace Interfaces;
-public class TeamScore
+public struct TeamScore
 {
     public Team team {get;}
     public int score {get;}
@@ -8,28 +8,37 @@ public class TeamScore
         this.team = team;
         this.score = score;
     }
-
+    public override bool Equals(object? obj)  => obj is TeamScore other && this.Equals(other);
+    public bool Equals(TeamScore otherTeamScore) => team == otherTeamScore.team && score == otherTeamScore.score;
+    public override int GetHashCode() => (team, score).GetHashCode();
+    public static bool operator ==(TeamScore lhs, TeamScore rhs) => lhs.Equals(rhs);
+    public static bool operator !=(TeamScore lhs, TeamScore rhs) => !(lhs == rhs);
+    
 }
-public class MatchScore
+public struct MatchScore
 {
-
-    TeamScore[] teamScore;
+    TeamScore teamOneScore;
+    TeamScore teamTwoScore;
     public MatchScore(Team teamOne, Team teamTwo, int scoreTeamOne, int scoreTeamTwo)
     {
-        teamScore = new TeamScore[]{
-            new TeamScore(teamOne, scoreTeamOne),
-            new TeamScore(teamTwo, scoreTeamTwo)
-            };
+        if (teamOne==teamTwo) throw new Exception("Should be different teams");
+        teamOneScore = new TeamScore(teamOne, scoreTeamOne);
+        teamTwoScore = new TeamScore(teamTwo, scoreTeamTwo);
     }
     public int GetTeamScore(Team team)
     {
-        var targetTeamScore = teamScore.FirstOrDefault(x=>x.team==team);
-        if (targetTeamScore == null) 
-            throw new Exception($"The team {team} did not participate in the match");
-        return targetTeamScore.score;
+        if (teamOneScore.team==team) return teamOneScore.score;
+        if (teamTwoScore.team==team) return teamTwoScore.score;
+        throw new Exception($"The team {team} did not participate in the match");
     }
     public override string ToString()
     {
-        return  $"({teamScore[0].score}, {teamScore[1].score})";
+        return  $"({teamOneScore.score}, {teamTwoScore.score})";
     }
+    public override bool Equals(object? obj)  => obj is MatchScore other && this.Equals(other);
+    public bool Equals(MatchScore otherMatchScore) => teamOneScore==otherMatchScore.teamOneScore&&teamTwoScore==otherMatchScore.teamTwoScore
+        || teamOneScore==otherMatchScore.teamTwoScore&&teamTwoScore==otherMatchScore.teamOneScore;
+    public override int GetHashCode() => (teamOneScore,teamTwoScore).GetHashCode();
+    public static bool operator ==(MatchScore lhs, MatchScore rhs) => lhs.Equals(rhs);
+    public static bool operator !=(MatchScore lhs, MatchScore rhs) => !(lhs == rhs);
 }
